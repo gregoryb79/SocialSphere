c8d78e807f49f033af089ce09cb939c90fe68aa4
gregoryb79 <gregoryb79@gmail.com>
2025-06-22 16:31:24 +0300

    not found added

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,6 +1,8 @@
+import styles from "./NotFound.module.scss";
+
 export function NotFound() {
   return (
-    <div>
+    <div className={styles.notFound}>
       <h1>404 - Not Found</h1>
       <p>The page you are looking for does not exist.</p>
     </div>

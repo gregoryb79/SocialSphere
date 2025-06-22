8a7caf7f348a8c5e4f7b041cd82f5ee35bfd3d1c
gregoryb79 <gregoryb79@gmail.com>
2025-06-22 16:45:41 +0300

    home page nicened

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,8 @@ import styles from "./Home.module.scss";
 import { useLoaderData } from "react-router";
 import { GeneralButton } from "./components/GeneralButton";
 import type { Post } from "../models/posts";
+import { NavButton } from "./components/NavButton";
+import { Bookmark, Heart,MessageCircle } from "lucide-react";
 
 
 export function Home() {
@@ -22,9 +24,14 @@ export function Home() {
                     <p><strong>Created at:</strong> {new Date(post.createdAt).toLocaleString()}</p>                    
                     {post.image && <img src={post.image} alt="Post visual content" className={styles.postImage} />}
                     <section className={styles.postActions}>
-                        <GeneralButton label="Like" onClick={() => console.log(`Liked post ${post._id}`)} />
-                        <GeneralButton label="Comment" onClick={() => console.log(`Commented on post ${post._id}`)} /> 
-                        <GeneralButton label="Bookmark" onClick={() => console.log(`Bookemarked on post ${post._id}`)} />             
+                        <section className={styles.likeComment}>
+                            <NavButton title="Like" label={post.likes.length.toString()} ariaLabel= "Like post" icon={<Heart className={styles.lucideIconPost} color="var(--primary-blue)"/>}
+                                onClick={() => console.log(`Liked post ${post._id}`)} />
+                            <NavButton title="Comment" label={post.comments.length.toString()} ariaLabel= "Add comment to post" icon={<MessageCircle className={styles.lucideIconPost} color="var(--primary-blue)"/>}
+                              onClick={() => console.log(`Commented on post ${post._id}`)} /> 
+                        </section>                        
+                        <NavButton title="Bookmark" ariaLabel= "Bookmark post" icon={<Bookmark className={styles.lucideIconPost} color="var(--primary-blue)"/>}
+                            onClick={() => console.log(`Bookemarked on post ${post._id}`)} />             
                     </section>
                 </li>))}
             </ul>

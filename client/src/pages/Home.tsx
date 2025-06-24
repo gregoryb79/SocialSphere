40bf6a99fb2d663d5a51554bce282d8bbcdeeb84
gregoryb79 <gregoryb79@gmail.com>
2025-06-24 12:01:14 +0300

    interactive feed display - can expand large posts and show and hide comments

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
 import styles from "./Home.module.scss";
 import { useLoaderData } from "react-router";
-import { GeneralButton } from "./components/GeneralButton";
 import type { Post } from "../models/posts";
-import { IconButton } from "./components/IconButton";
-import { Bookmark, Heart,MessageCircle, User } from "lucide-react";
+import { User } from "lucide-react";
+import { PostCard } from "./components/PostCard";
 
 
 export function Home() {
@@ -21,25 +19,10 @@ export function Home() {
                 <h2>{username}</h2>
             </section>
             <ul className={styles.postsFeed}>
-                {posts.map((post) => (
-                <li key={post._id} className={styles.postCard}>                     
-                    <p>{post.content}</p>
-                    <p><strong>Author:</strong> {post.author}</p>
-                    <p><strong>Created at:</strong> {new Date(post.createdAt).toLocaleString()}</p>                    
-                    {post.image && <img src={post.image} alt="Post visual content" className={styles.postImage} />}
-                    <section className={styles.postActions}>
-                        <section className={styles.likeComment}>
-                            <IconButton title="Like" label={post.likes.length.toString()} ariaLabel= "Like post" icon={<Heart className={styles.lucideIconPost} color="var(--primary-blue)"/>}
-                                onClick={() => console.log(`Liked post ${post._id}`)} />
-                            <IconButton title="Comment" label={post.comments.length.toString()} ariaLabel= "Add comment to post" icon={<MessageCircle className={styles.lucideIconPost} color="var(--primary-blue)"/>}
-                              onClick={() => console.log(`Commented on post ${post._id}`)} /> 
-                        </section>                        
-                        <IconButton title="Bookmark" ariaLabel= "Bookmark post" icon={<Bookmark className={styles.lucideIconPost} color="var(--primary-blue)"/>}
-                            onClick={() => console.log(`Bookemarked on post ${post._id}`)} />             
-                    </section>
-                </li>))}
+                {posts.map((post) => (<PostCard key={post._id} post={post} />))}
             </ul>
             
         </main>
     );
 }
+

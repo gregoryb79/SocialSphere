d23c30c8addfb555c912ee7432a9d6885151d455
Coral Tzubery <tzubery.coral@gmail.com>
2025-07-01 21:25:21 +0300

    removing the mongo db code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import dotenv from "dotenv";
 import { createServer } from "http";
-// import mongoose from "mongoose";
 import { app } from "./app";
 import { dbClient } from "./models/db";
 
@@ -25,17 +24,3 @@ async function init() {
 }
 
 init();
-
-// dotenv.config({ path: "../.env" });
-// const server = createServer(app);
-// const port = process.env.PORT || 5000;
-
-// async function init() {
-//     await mongoose.connect(process.env.CONNECTION_STRING!, {
-//         dbName: process.env.DB_NAME,
-//     });
-
-//     server.listen(port, () => console.log(`Server listening on port ${port}`));
-// }
-
-// init();

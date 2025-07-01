cd8c2bdb063cbfe188736f34c7d7e7546d2652b0
Coral Tzubery <tzubery.coral@gmail.com>
2025-07-01 21:24:05 +0300

    connecting turso

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,41 @@
 import "dotenv/config";
 import dotenv from "dotenv";
-
-
 import { createServer } from "http";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import { app } from "./app";
+import { dbClient } from "./models/db";
 
-dotenv.config({ path: "../.env" });
+dotenv.config();
+
 const server = createServer(app);
 const port = process.env.PORT || 5000;
 
 async function init() {
-    await mongoose.connect(process.env.CONNECTION_STRING!, {
-        dbName: process.env.DB_NAME,
-    });
+    try {
+        const result = await dbClient.execute("SELECT 1 as test");
+        console.log("Connected to Turso:", result.rows);
 
-    server.listen(port, () => console.log(`Server listening on port ${port}`));
+        server.listen(port, () => {
+            console.log(`Server listening on port ${port}`);
+        });
+    } catch (error) {
+        console.error("Faild to connect to Turso:", error);
+        process.exit(1);
+    }
 }
 
 init();
+
+// dotenv.config({ path: "../.env" });
+// const server = createServer(app);
+// const port = process.env.PORT || 5000;
+
+// async function init() {
+//     await mongoose.connect(process.env.CONNECTION_STRING!, {
+//         dbName: process.env.DB_NAME,
+//     });
+
+//     server.listen(port, () => console.log(`Server listening on port ${port}`));
+// }
+
+// init();

f7cf8abe016995e38acd655df9c4ba1cd2119590
Coral Tzubery <tzubery.coral@gmail.com>
2025-07-08 19:04:16 +0300

    load previous chat messages

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,8 @@ import { json } from "body-parser";
 import { router as usersRouter} from "./routers/users.router";
 import { router as postsRouter } from "./routers/posts.router";
 import { router as commentsRouter} from "./routers/comments.router";
-import { router as notificationRouter } from "./routers/notifications.route";  
+import { router as notificationRouter } from "./routers/notifications.route"; 
+import { router as chatRouter } from "./routers/chat.route"; 
 import cors from "cors";
 
 export const app = express();
@@ -24,8 +25,5 @@ app.use(json());
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 app.use("/comments", commentsRouter);
-app.use("/api/notifications", notificationRouter)
-
-
-
-
+app.use("/notifications", notificationRouter)
+app.use("/chat", chatRouter);

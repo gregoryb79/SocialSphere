c79947f9a4d91ed5d83a1648639971ee7c98aae3
Coral Tzubery <tzubery.coral@gmail.com>
2025-07-08 18:49:04 +0300

    connecting the chat to sql

--- server/src/chat.ts
+++ server/src/chat.ts
@@ -1,5 +1,7 @@
 import { Server } from "socket.io";
 import http from "http";
+import { dbClient } from "./models/db";
+import { randomUUID } from "crypto";
 
 export function setupChatSocket(server: http.Server) {
     const io = new Server(server, {
@@ -12,8 +14,14 @@ export function setupChatSocket(server: http.Server) {
     io.on("connection", (socket) => {
         console.log("Client connected:", socket.id);
 
-        socket.on("message", (data) => {
+        socket.on("message", async (data) => {
             console.log("Message receivrd:", data);
+
+            await dbClient.execute({
+                sql: `INSERT INTO messages (id, chat_id, sender_id, receiver_id, text, created_at) VALUES (?, ?, ?, ?, ?, ?)`,
+                args: [randomUUID(), data.chat_id, data.sender_id, data.receiver_id, data.text, new Date().toISOString()]
+            });
+
             io.emit("message", data);
         });
 

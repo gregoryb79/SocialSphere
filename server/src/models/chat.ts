e5582d11973c739d6a986ac2d44889d832f2ed64
Coral Tzubery <tzubery.coral@gmail.com>
2025-07-12 12:28:45 +0300

    adding friends models

--- server/src/models/chat.ts
+++ server/src/models/chat.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from "crypto";
 import { dbClient } from "./db";
+import { areFriends } from "./friends";
 
 export type ChatMessage = {
     chat_id: string;
@@ -9,6 +10,12 @@ export type ChatMessage = {
 };
 
 export async function saveMessageToDb(message: ChatMessage) {
+    const friends = await areFriends(message.sender_id, message.receiver_id);
+
+    if (!friends) {
+        throw new Error("Users are not friends. Message not saved.");
+    }
+    
     await dbClient.execute({
         sql: `INSERT INTO messages (id, chat_id, sender_id, receiver_id, text, created_at)
         VALUES (?, ?, ?, ?, ?, ?)`,

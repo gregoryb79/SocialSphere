e382df7aec2bfb30f7be1986ba5e25bd1b582456
OfirKadosh <ofir.kadoshosh@gmail.com>
2025-07-12 10:25:52 +0300

    resolved conflicts in users.router.ts

--- server/src/routers/users.router.ts
+++ server/src/routers/users.router.ts
@@ -3,6 +3,12 @@ import { authenticate } from "../middlewares/auth.middleware";
 import { followUser, unfollowUser } from "../controllers/user.controller";
 
 export const router = express.Router();
+import { dbClient } from "../models/db";
+
+router.get("/", async (_, res) => {
+    res.status(200).json({
+        message: "Welcome to the SocialSphere users API",});
+});
 
 router.post("/:id/follow", authenticate, followUser);
 router.post("/:id/unfollow", authenticate, unfollowUser);
